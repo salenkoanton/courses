8a6e4b1fcf4caeb32c69404bd6b0f44184081608
salenkoanton <salenko3110@gmail.com>
2015-10-10 09:53:49 +0300

    3514

--- courses/prog_base/labs/lab1/math.c
+++ courses/prog_base/labs/lab1/math.c
@@ -2,11 +2,11 @@
 double calc (double x, double y, double z)
 {
 	double a = 0;
-	if ((x <= y) || (z == 0) || ( x <= 0 ) || (sin(x) == 0) || (x - y == 0)) 
+	if ((x <= y) || (z == 0) || ( x <= 0 ) || (sin(x) == 0)) 
 		return NAN;
 	else 
 	{
-		a = pow(x, y + 1) / (pow(x - y, 1 / z)) + y / (3 * fabs(x + y)) + sqrt(fabs(cos(y) / sin(x) + 1));
+		a = pow(x, y + 1) / (pow(x - y, 1 / z)) + 3 * y + z / x + sqrt(fabs(cos(y) / sin(x) + 1));
 		return a;
 	}
 }
