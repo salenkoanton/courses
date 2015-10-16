9aae6e92cc5d49b044d6dff213d0d21fada30a9a
salenkoanton <salenko3110@gmail.com>
2015-10-16 16:20:02 +0300

    5165

--- courses/prog_base/tasks/long2double/repr.c
+++ courses/prog_base/tasks/long2double/repr.c
@@ -20,6 +20,10 @@ double long2double(long long a)
 	{
 		arr[i] = ((1LL << i) & a) >> i;
 	}
+	a = 0;
+	for (i = 52; i < 63; i++)
+	if (arr[i] == 1)
+		a += pow(2, i - 52);
 	for (i = 51; i > -1; i--)
 	{
 		if (arr[i] == 1)
@@ -27,35 +31,29 @@ double long2double(long long a)
 	}
 	if (arr[63] == -1) 
 		b = -b;
-	a = 0;
+	
 	for (i = 0; i < 64 ; i++)
 		if (arr[i] == 1) 
 			e = 1;
-	if ((e == 0) && (arr[63] == 0))
-		return 0;
-	if ((e == 0) && (arr[63] == -1))
-		return -0;
-	for (i = 52; i < 63; i++)
-		if (arr[i] == 1)
-			a += pow(2, i - 52);
+
+
 	if (a == 0)
 		return b - 1;
 	printf(" %li ", a);
 	b *= pow(2, (a - 1023));
-
+	if ((e == 0) && (arr[63] == 0))
+		return 0;
+	if ((e == 0) && (arr[63] == -1))
+		return -0;
 	if (a == 2047)
 	{
-		if (b == 1)
-		{
-			if (arr[63] == -1)
-			
-				return -INFINITY;
-			else 
-				return INFINITY;
-		}
-		else 
+		e = 0;
+		for (i = 0; i < 52 ; i++)
+		if (arr[i] == 1) 
+			e = 1;
+		if (e == 1)
 			return NAN;
-				
+		return b;
 	}
 	return b;
 }
